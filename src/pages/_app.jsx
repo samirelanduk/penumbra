e332b7b8c52cc41c03afa8760d0a22d4c616b5e6
Sam Ireland <mail@samireland.com>
2023-09-28 00:42:41 +0100

    Basic favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,10 +2,14 @@ import BottomRow from "@/components/BottomRow";
 import TextEditor from "@/components/TextEditor";
 import TopRow from "@/components/TopRow";
 import "@/index.css";
+import Head from "next/head";
 
 export default function App() {
   return (
     <div className="h-screen flex flex-col">
+      <Head>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
       <TopRow />
       <TextEditor />
       <BottomRow />
