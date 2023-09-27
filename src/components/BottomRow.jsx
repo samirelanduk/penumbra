9b90a9b34fabffde445d4d54de150015e3bb5f76
Sam Ireland <mail@samireland.com>
2023-09-28 00:52:17 +0100

    Word counts

--- src/components/BottomRow.jsx
+++ src/components/BottomRow.jsx
@@ -6,9 +6,14 @@ const BottomRow = props => {
 
   const charCount = text.length;
 
+  const words = text.match(/\b\w+\b/g);
+  const wordCount = words ? words.length : 0;
+    
   return (
     <div className="w-full border-t h-10 flex items-center text-sm text-gray-500 px-4">
       <div>{charCount} character{charCount === 1 ? "" : "s"} </div>
+      <div className="mx-2">|</div>
+      <div>{wordCount} word{wordCount === 1 ? "" : "s"}</div>
     </div>
   );
 };
