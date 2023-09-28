829b9018b06c1cedec9391a6b89403b7a6fa7229
Sam Ireland <mail@samireland.com>
2023-09-28 01:52:43 +0100

    Auto-focus

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,12 +1,20 @@
+import { useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 
 const TextEditor = props => {
 
   const { text, setText } = props;
 
+  const ref = useRef(null);
+  
+  useEffect(() => {
+    ref.current.focus();
+  }, []);
+
   return (
     <div className="flex-grow ">
       <textarea
+        ref={ref}
         value={text}
         onChange={e => setText(e.target.value)}
         className="w-full block h-full px-4 outline-none py-4 resize-none max-w-none prose lg:prose-xl md:px-[calc((100vw-738px)/2)] lg:px-[calc((100vw-800px)/2)] dark:bg-slate-800 dark:text-slate-200"
