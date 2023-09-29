357965997f1ebbb74c6ae26be693f51fe3e7f482
Sam Ireland <mail@samireland.com>
2023-09-29 22:00:36 +0100

    SEO tags

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -16,11 +16,21 @@ export default function App() {
 
   const [text, setText] = useState("");
 
+  const title = "Penumbra - encrypted local notes";
+  const description = "Penumbra is a simple, secure, and private note-taking app. It runs entirely in your browser, and your notes are never sent to a server. You can even use it offline.";
+
   return (
     <div className={`${mulish.variable}`}>
       <Head>
         <link rel="icon" href="/favicon.svg" />
         <link rel="manifest" href="/manifest.json" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta content="@samirelanduk" name="twitter:site"/>
+        <meta content={title} name="twitter:title"/>
+        <meta content={description} name="twitter:description"/>
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
       </Head>
       <div className="h-screen flex flex-col font-sans">
         <TopRow />
