fe3e8cb1ab5e72432716af20b50666af5537edc0
Sam Ireland <mail@samireland.com>
2023-10-02 19:20:58 +0100

    Text file saving

--- src/components/TopRow.jsx
+++ src/components/TopRow.jsx
@@ -18,10 +18,25 @@ const TopRow = props => {
     setDocument({text: contents, name: fileHandle.name});
   }
 
+  const saveClicked = async () => {
+    let fileHandle;
+    try {
+      fileHandle = await window.showSaveFilePicker({
+        types: [{description: "Text", accept: {"text/*": [".txt", ".md"]}}],
+        suggestedName: document.name || "Untitled.txt",
+      });
+    } catch { return }
+    const writable = await fileHandle.createWritable();
+    await writable.write(document.text);
+    await writable.close();
+    setDocument({...document, name: fileHandle.name});
+  }
+
   return (
     <div className="w-full flex items-center justify-center px-6 border-b h-10 relative dark:border-gray-600">
       <div className="text-gray-500 text-sm">{document ? document.name : ""}</div>
       <div onClick={openClicked} className="cursor-pointer absolute right-6">Open</div>
+      <div onClick={saveClicked} className="cursor-pointer absolute right-24">Save</div>
     </div>
   );
 };
