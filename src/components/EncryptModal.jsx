c2115be4f7dac4364250bc6092b335e4e907777a
Sam Ireland <mail@samireland.com>
2023-10-12 11:11:55 +0100

    Style encryption modal

--- src/components/EncryptModal.jsx
+++ src/components/EncryptModal.jsx
@@ -29,37 +29,52 @@ const EncryptModal = props => {
     setShow(false);
   }
 
+  const width = "w-60";
+  const labelClass = "text-sm mb-1";
+  const inputClass = `${width} block appearance-none border rounded py-1.5 px-3 text-gray-700 mb-5 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-200`;
+  const buttonClass = "py-1 px-4 w-full text-sm rounded focus:outline-none focus:shadow-outline";
+  const encryptButtonClass = `${buttonClass} bg-orange-400 hover:bg-orange-500 text-white dark:bg-orange-800 dark:hover:bg-orange-700`;
+  const cancelButtonClass = `${buttonClass} border-2 border-orange-400 hover:border-orange-500 text-orange-500 hover:text-orange-700 dark:border-orange-800 dark:text-orange-700 dark:hover:border-orange-700 dark:hover:text-orange-600`;
+
   return (
     <Modal setShow={setShow}>
-      <div>Encrypting</div>
-      <div className="flex flex-col">
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
+      <div className="text-lg mb-4 border-b border-orange-100 py-4 px-6 text-gray-800 dark:text-slate-300 dark:border-orange-900">
+        Encrypting as new file
       </div>
-      <div className="flex flex-col">
-        <label htmlFor="confirmPassword">Confirm Password</label>
-        <input
-          id="confirmPassword"
-          type="password"
-          value={confirmPassword}
-          onChange={e => setConfirmPassword(e.target.value)}
-        />
+      <div className="px-6 pt-2 pb-6">
+        <div className="flex flex-col">
+          <label htmlFor="password" className={labelClass}>Password</label>
+          <input
+            id="password"
+            type="password"
+            className={inputClass}
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col mb-2">
+          <label htmlFor="confirmPassword" className={labelClass}>Confirm Password</label>
+          <input
+            id="confirmPassword"
+            type="password"
+            className={inputClass}
+            value={confirmPassword}
+            onChange={e => setConfirmPassword(e.target.value)}
+          />
+        </div>
+        <div className={`flex ${width} gap-4 justify-between`}>
+          <button
+            onClick={encryptClicked}
+            className={`${encryptButtonClass} ${canEncrypt ? "" : "opacity-50 pointer-events-none"}`}
+            disabled={!canEncrypt}
+          >
+            Encrypt
+          </button>
+          <button onClick={() => setShow(false)} className={cancelButtonClass}>
+            Cancel
+          </button>
+        </div>
       </div>
-      <button
-        className="rounded-md bg-slate-500 text-white px-4 py-2"
-        disabled={!canEncrypt}
-        onClick={encryptClicked}
-      >
-        Encrypt
-      </button>
-      <button onClick={() => setShow(false)}>
-        Cancel
-      </button>
     </Modal>
   );
 };
