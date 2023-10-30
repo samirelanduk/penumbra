e157c62e682455422b16efe87df36d2d8be2ac55
Sam Ireland <mail@samireland.com>
2023-10-30 23:22:19 +0000

    Test serialize.js

---
+++ src/serialize.test.js
@@ -0,0 +1,31 @@
+import { plainText } from "./serialize";
+
+describe("plainText", () => {
+
+  it("Returns empty string for empty document", async () => {
+    const nodes = [];
+    expect(plainText(nodes)).toEqual("");
+  })
+
+  it("Returns plain text for a single paragraph", async () => {
+    const nodes = [{type: "p", children: [{ text: "Hello world"}]}];
+    expect(plainText(nodes)).toEqual("Hello world");
+  })
+
+  it("Returns plain text for multiple paragraphs", async () => {
+    const nodes = [
+      {type: "p", children: [{ text: "Hello world"}]},
+      {type: "p", children: [{ text: "Line2 line2."}]}
+    ];
+    expect(plainText(nodes)).toEqual("Hello world\nLine2 line2.");
+  })
+
+  it("Handles multiple inline elements", async () => {
+    const nodes = [
+      {type: "p", children: [{ text: "Hello world "}, { text: "Line2 line2."}]},
+      {type: "p", children: [{ text: "Line2 line2."}]}
+    ];
+    expect(plainText(nodes)).toEqual("Hello world Line2 line2.\nLine2 line2.");
+  })
+
+})
