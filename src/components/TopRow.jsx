ba0d4bc59be4e96c2c199416aade8cffa3a876fc
Sam Ireland <mail@samireland.com>
2023-10-30 21:02:30 +0000

    Remove margin top if first block is paragraph

--- src/components/TopRow.jsx
+++ src/components/TopRow.jsx
@@ -7,7 +7,7 @@ const TopRow = props => {
   const { document, setDocument } = props;
 
   return (
-    <div className="w-full flex justify-start flex-shrink-0 items-center px-6 h-12 relative sm:justify-center">
+    <div className="w-full flex justify-start flex-shrink-0 items-center px-4 h-12 relative sm:justify-center">
       <Toolbar />
       <Menu document={document} setDocument={setDocument} />
     </div>
