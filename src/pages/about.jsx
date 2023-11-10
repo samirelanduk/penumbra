7ee79f11a739ba48af19110f77b705a106cf2fad
Sam Ireland <mail@samireland.com>
2023-11-10 23:21:32 +0000

    Plausible on about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Features from "@/components/about/Features";
 import FrequentlyAskedQuestions from "@/components/about/FrequentlyAskedQuestions";
 import Hero from "@/components/about/Hero";
@@ -5,6 +6,18 @@ import Footer from "@/components/about/Footer";
 import Nav from "@/components/about/Nav";
 
 export default function About() {
+
+  useEffect(() => {
+    const hostnames = ["penumbra.app", "www.penumbra.app"];
+    const currentDomain = window.location.hostname;
+    if (hostnames.includes(currentDomain)) {
+      const script = document.createElement("script");
+      script.defer = true;
+      script.dataset.domain = "plausible.app";
+      script.src = "https://plausible.io/js/script.js";
+      document.body.appendChild(script);
+    }
+  }, []);
 
   return (
     <div className="font-sans">
