c18fc7a01c4d631ff6a52e6c9043c4cf8d703a6d
Sam Ireland <mail@samireland.com>
2023-12-07 12:27:17 +0000

    Extra toolbar icons

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -9,6 +9,9 @@ import H1Icon from "@/images/h1.svg";
 import H2Icon from "@/images/h2.svg";
 import H3Icon from "@/images/h3.svg";
 import BulletIcon from "@/images/bullet.svg";
+import NumberIcon from "@/images/numbers.svg";
+import QuoteIcon from "@/images/quote.svg";
+import CodeIcon from "@/images/code.svg";
 import { useSlate } from "slate-react";
 import { useContext } from "react";
 import { PreviewContext } from "@/contexts";
@@ -22,6 +25,7 @@ const Toolbar = () => {
     "1": () => toggleBlock(editor, "h1"),
     "2": () => toggleBlock(editor, "h2"),
     "3": () => toggleBlock(editor, "h3"),
+    "`": () => toggleBlock(editor, "code"),
     "b": () => toggleMark(editor, "bold"),
     "i": () => toggleMark(editor, "italics"),
     "u": () => toggleMark(editor, "underline"),
@@ -67,8 +71,11 @@ const Toolbar = () => {
       checker: isBlockActive,
       toggle: toggleBlock,
       buttons: [
-        {type: "p", icon: <ParagraphIcon className={iconClass} />, className: `p-1 ${preview ? "" : "sm:p-2"}`},
+        {type: "p", icon: <ParagraphIcon className={iconClass} />, className: `p-1.5 ${preview ? "" : "sm:p-2"}`},
         {type: "ul", icon: <BulletIcon className={iconClass} />, className: `p-1 ${preview ? "" : "sm:p-1.5"}`},
+        {type: "ol", icon: <NumberIcon className={iconClass} />, className: `p-1.5 ${preview ? "" : "sm:p-2"}`},
+        {type: "blockquote", icon: <QuoteIcon className={iconClass} />, className: `p-1.5 ${preview ? "" : "sm:p-2"}`},
+        {type: "code", icon: <CodeIcon className={iconClass} />, className: `p-1 ${preview ? "" : "sm:p-1.5"}`},
         {type: "h1", icon: <H1Icon className={iconClass} />, className: preview ? "" : "sm:p-0.5"},
         {type: "h2", icon: <H2Icon className={iconClass} />, className: `p-0.5 ${preview ? "" : "sm:p-1"}`},
         {type: "h3", icon: <H3Icon className={iconClass} />, className: `p-1 ${preview ? "" : "sm:p-1.5"}`},
@@ -78,16 +85,16 @@ const Toolbar = () => {
       checker: isMarkActive,
       toggle: toggleMark,
       buttons: [
-        {type: "bold", icon: <BoldIcon className={iconClass} />, className: `p-1 ${preview ? "" : "sm:p-2"}`},
-        {type: "italics", icon: <ItalicsIcon className={iconClass} />, className: `p-1 ${preview ? "" : "sm:p-2"}`},
-        {type: "underline", icon: <UnderlineIcon className={iconClass} />, className: `p-1 ${preview ? "" : "sm:p-2"}`},
-        {type: "strikethrough", icon: <StrikethroughIcon className={iconClass} />, className: `p-0.5 ${preview ? "" : "sm:p-1.5"}`},
+        {type: "bold", icon: <BoldIcon className={iconClass} />, className: `p-1.5 ${preview ? "" : "sm:p-2"}`},
+        {type: "italics", icon: <ItalicsIcon className={iconClass} />, className: `p-1.5 ${preview ? "" : "sm:p-2"}`},
+        {type: "underline", icon: <UnderlineIcon className={iconClass} />, className: `p-1.5 ${preview ? "" : "sm:p-2"}`},
+        {type: "strikethrough", icon: <StrikethroughIcon className={iconClass} />, className: `p-1 ${preview ? "" : "sm:p-1.5"}`},
       ],
     }
   ];
 
   return (
-    <div className={`flex gap-3 w-fit ${preview ? "scale-75 -ml-8 sm:scale-100 sm:ml-0" : "sm:gap-5"}`}>
+    <div className={`flex flex-col gap-x-3 gap-y-1.5 w-fit sm:flex-row ${preview ? "scale-75 -ml-8 sm:flex-col sm:scale-100 sm:ml-0" : "sm:gap-5"}`}>
       {blocks.map((block, index) => {
         return (
           <div className={`flex gap-1 ${preview ? "" : "sm:gap-2"}`} key={index}>
--- src/components/TopRow.jsx
+++ src/components/TopRow.jsx
@@ -11,7 +11,7 @@ const TopRow = props => {
   const preview = useContext(PreviewContext);
 
   return (
-    <div className={`w-full flex justify-start flex-shrink-0 items-center px-4 h-12 relative ${preview ? "" : "sm:justify-center"}`}>
+    <div className={`w-full flex justify-start flex-shrink-0 items-center px-4 h-16 pt-1 relative sm:h-12 sm:pt-0 ${preview ? "sm:h-16 sm:pt-1" : "sm:justify-center"}`}>
       <Toolbar />
       <Menu document={document} setDocument={setDocument} />
     </div>
